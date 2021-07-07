e94bd501e64fc6d4b484e7f269d61146136ba5f2
Isaque Kaio <isaque.araujo@escolar.ifrn.edu.br>
2021-07-07 14:54:27 -0300

    Implementation screen for edit book.

---
+++ app/src/main/java/com/example/bookmanager/views/EditBookActivity.java
@@ -0,0 +1,23 @@
+package com.example.bookmanager.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookmanager.R;
+
+public class EditBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book);
+    }
+
+    public void cancel(View view) {
+    }
+
+    public void process(View view) {
+    }
+}
